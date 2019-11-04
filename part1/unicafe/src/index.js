597e2e24b174892194e07d807b44593fd7a6ed1b
Dominik Peuster <dominikpe@unitybox.de>
2019-11-04 19:32:24 +0100

    Exercise 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,7 +9,10 @@ const Button = ({onClick, text}) => {
 
 const Statistic = ({text, value}) => {
     return (
-        <p>{text} {value}</p>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     );
 };
 
@@ -23,14 +26,16 @@ const Statistics = (props) => {
     }
     else {
         content = (
-            <>
-                <Statistic text="good" value={good}/>
-                <Statistic text="neutral" value={neutral}/>
-                <Statistic text="bad" value={bad}/>
-                <Statistic text="all" value={all}/>
-                <Statistic text="average" value={average}/>
-                <Statistic text="positive" value={positive}/>
-            </>
+            <table>
+                <tbody>
+                    <Statistic text="good" value={good}/>
+                    <Statistic text="neutral" value={neutral}/>
+                    <Statistic text="bad" value={bad}/>
+                    <Statistic text="all" value={all}/>
+                    <Statistic text="average" value={average}/>
+                    <Statistic text="positive" value={positive}/>
+                </tbody>
+            </table>
         );
     }
 

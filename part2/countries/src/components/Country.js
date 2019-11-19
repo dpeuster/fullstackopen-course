23315b82e7d368dcf23351dc7b50e5968905b706
Dominik Peuster <dominik.peuster@stadt.wuppertal.de>
2019-11-19 13:16:10 +0100

    Started Exercise 2.14 (apikey for openweathermap is not yet activated so could not try yet

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Country = ({ country }) => {
+    const [ weather, setWeather ] = useState({});
+
+    const weatherApiKey = 'b9c2fb61a4c3fbbe5af73adabb138c09';
+    const weatherApiLink = `http://api.openweathermap.org/data/2.5/weather?appid=${weatherApiKey}&q=`;
+
+    useEffect(() => {
+        axios
+            .get(`${weatherApiLink}${country.capital}`)
+            .then(({ response: { data }}) => {
+                setWeather(data);
+
+                console.log(data);
+            })
+    }, []);
+
     return (
         <div>
             <h1>{country.name}</h1>
@@ -11,7 +27,7 @@ const Country = ({ country }) => {
             </p>
             <h2>languages</h2>
             <ul>
-                {country.languages.map(language => <li>{language.name}</li>)}
+                {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
             </ul>
             <img src={country.flag} width="250" />
         </div>
